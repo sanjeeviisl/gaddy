7dd6dc061eb440446cde7694dc1a237b4052d144
sanjev kumar <sanjeevkmsinha@gmail.com>
2017-03-22 14:23:43 +0530

    updated

--- control_panel/promotions/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_host="localhost";
-$mysql_user="gaddy_root";
-$mysql_pass="gaddy8jss@";
-$mysql_database="gaddydatabase";
-$link=mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_database);
-
-?>
--- gaddy_control_panel/classes/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_host="localhost";
-$mysql_user="tracker_root";
-$mysql_pass="Rajat8st@#";
-$mysql_database="gaddytrackerdatabase";
-$link=mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_database);
-
-?>
